bf5f808096050bc883320ef3b28bbaeee0156715
mangosata <mangosata@gmail.com>
2020-07-28 18:32:51 -0400

    Added redux thunk and loading

--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -1,4 +1,15 @@
-import { CREATE_EVENT, UPDATE_EVENT, DELETE_EVENT } from "./eventConstants";
+import {
+  CREATE_EVENT,
+  UPDATE_EVENT,
+  DELETE_EVENT,
+  FETCH_EVENTS,
+} from "./eventConstants";
+import {
+  asyncActionStart,
+  asyncActionFinish,
+  asyncActionError,
+} from "../async/asyncActions";
+import { fetchSampleData } from "../../app/data/mockApi";
 
 export const createEvent = (event) => {
   return {
@@ -7,17 +18,30 @@ export const createEvent = (event) => {
   };
 };
 
-
 export const updateEvent = (event) => {
-    return {
-        type: UPDATE_EVENT,
-        payload: { event },
-      };
-}
+  return {
+    type: UPDATE_EVENT,
+    payload: { event },
+  };
+};
 
 export const deleteEvent = (eventId) => {
-    return {
-        type: DELETE_EVENT,
-        payload: { eventId },
-      };
-}
+  return {
+    type: DELETE_EVENT,
+    payload: { eventId },
+  };
+};
+
+export const loadEvents = () => {
+  return async (dispatch) => {
+    try {
+      dispatch(asyncActionStart());
+      const events = await fetchSampleData();
+      dispatch({ type: FETCH_EVENTS, payload: { events } });
+      dispatch(asyncActionFinish());
+    } catch (error) {
+      console.log(error);
+      dispatch(asyncActionError());
+    }
+  };
+};
--- src/features/nav/NavBar/NavBar.jsx
+++ src/features/nav/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { connect } from "react-redux";
 import { Menu, Container, Button } from "semantic-ui-react";
 import { NavLink, Link, withRouter } from "react-router-dom";
@@ -41,17 +41,21 @@ class NavBar extends Component {
             Re-vents
           </Menu.Item>
           <Menu.Item exact as={NavLink} to="/events" name="Events" />
-          <Menu.Item as={NavLink} to="/people" name="People" />
-          <Menu.Item>
-            <Button
-              as={Link}
-              to="/createEvent"
-              floated="right"
-              positive
-              inverted
-              content="Create Event"
-            />
-          </Menu.Item>
+          {authenticated && (
+            <Fragment>
+              <Menu.Item as={NavLink} to="/people" name="People" />
+              <Menu.Item>
+                <Button
+                  as={Link}
+                  to="/createEvent"
+                  floated="right"
+                  positive
+                  inverted
+                  content="Create Event"
+                />
+              </Menu.Item>
+            </Fragment>
+          )}
           {authenticated ? (
             <SignedInMenu
               signOut={this.handleSignOut}
