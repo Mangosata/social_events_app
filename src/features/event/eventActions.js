e06a9b699d69c4d4f62c4604447b2a8bd85d85f7
mangosata <mangosata@gmail.com>
2020-08-03 23:49:25 -0400

    Completed cloud functions

--- src/features/event/eventActions.js
+++ src/features/event/eventActions.js
@@ -81,14 +81,14 @@ export const getEventsForDashboard = (lastEvent) => async (
 
     lastEvent
       ? (query = eventsRef
-          // .where("date", ">=", today)
+          .where("date", ">=", today)
           .orderBy("date")
           .startAfter(startAfter)
-          .limit(2))
+          .limit(3))
       : (query = eventsRef
-          // .where("date", ">=", today)
+          .where("date", ">=", today)
           .orderBy("date")
-          .limit(2));
+          .limit(3));
 
     let querySnap = await query.get();
 
